7a79a6e6a1642749159c5c36f856944e79fb9160
Pavel <impetus@yandex.ru>
2021-02-20 11:18:22 +0300

    Example 3.9.1

---
+++ desktop/src/ru/samsung/itschool/mdev/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.samsung.itschool.mdev.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.samsung.itschool.mdev.SimpleGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SimpleGame(), config);
+	}
+}
